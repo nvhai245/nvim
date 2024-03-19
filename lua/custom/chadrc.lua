7c815e77c5d7aa55bb8e4bc23e7ef00e4e993ff9
Harry Nguyen <nvhai245@gmail.com>
2024-03-19 20:24:48 +0700

    updated statusline theme

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,13 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'wombat', telescope = { style = "bordered" } }
+M.ui = {
+  theme = 'wombat',
+  telescope = { style = "bordered" },
+  statusline = {
+    theme = "minimal"
+  }
+}
 M.plugins = "custom.plugins"
 M.mappings = require("custom.mappings")
 
