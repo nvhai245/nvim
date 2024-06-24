4185e42990577c9b28521bbb2e20d211e9a10b9e
Hai Nguyen <nvhai245@gmail.com>
2024-06-24 23:00:31 +0700

    removed hologram.nvim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -188,15 +188,6 @@ local plugins = {
     end
   },
   {
-    "edluffy/hologram.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("hologram").setup({
-        auto_display = true,
-      })
-    end
-  },
-  {
     "ThePrimeagen/vim-be-good",
     event = "VeryLazy"
   }
