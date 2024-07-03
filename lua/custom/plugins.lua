9deffa884ce17412e625cf6541442c73a3946737
Hai Nguyen <nvhai245@gmail.com>
2024-07-03 18:54:50 +0700

    updated supermaven keybinds

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -168,6 +168,8 @@ local plugins = {
       require("supermaven-nvim").setup({
         keymaps = {
           accept_suggestion = "<C-g>",
+          clear_suggestion = "<M-]>",
+          accept_word = "<M-j>",
         },
       })
     end,
